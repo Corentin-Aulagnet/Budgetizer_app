09a725844f2f696e00b704fe5bc8a306321c489a
Corentin-Aulagnet <corentin.aulagnet@gmail.com>
2023-04-03 21:45:54 -0700

    Personalized the aboutListTile

--- lib/navigation_drawer.dart
+++ lib/navigation_drawer.dart
@@ -5,6 +5,25 @@ import 'package:ledgerstats/app_colors.dart';
 class AppNavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    TextStyle style = theme.textTheme.bodyMedium!;
+    final List<Widget> aboutBoxChildren = <Widget>[
+      const SizedBox(height: 24),
+      RichText(
+          text: TextSpan(
+        children: <TextSpan>[
+          TextSpan(
+              style: style,
+              text: "Flutter is Google's UI toolkit for building beautiful, "
+                  'natively compiled applications for mobile, web, and desktop '
+                  'from a single codebase. Learn more about Flutter at '), //TODO localization
+          TextSpan(
+              style: style.copyWith(color: theme.colorScheme.primary),
+              text: 'https://flutter.dev'),
+          TextSpan(style: style, text: '.'),
+        ],
+      ))
+    ];
     return Drawer(
         child: Column(
       children: [
@@ -46,30 +65,10 @@ class AppNavigationDrawer extends StatelessWidget {
         AboutListTile(
             icon: const Icon(Icons.info),
             applicationIcon: const FlutterLogo(),
-            applicationName: 'Show About Example',
-            applicationVersion: 'August 2019',
-            applicationLegalese: '\u{a9} 2014 The Flutter Authors',
-            aboutBoxChildren: <Widget>[
-              const SizedBox(height: 24),
-              RichText(
-                  text: TextSpan(
-                children: <TextSpan>[
-                  TextSpan(
-                      style: Theme.of(context).textTheme.bodyMedium!,
-                      text:
-                          "Flutter is Google's UI toolkit for building beautiful, "
-                          'natively compiled applications for mobile, web, and desktop '
-                          'from a single codebase. Learn more about Flutter at '),
-                  TextSpan(
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                          color: Theme.of(context).colorScheme.primary),
-                      text: 'https://flutter.dev'),
-                  TextSpan(
-                      style: Theme.of(context).textTheme.bodyMedium!,
-                      text: '.'),
-                ],
-              ))
-            ]),
+            applicationName: 'LedgerStats',
+            applicationVersion: '1.0.0 April 2023', //TODO localization
+            applicationLegalese: '\u{a9} 2023 Corentin Aulagnet',
+            aboutBoxChildren: aboutBoxChildren),
         ListTile(
             leading: Icon(Icons.settings),
             title: const Text("Options"), //TODO localization
